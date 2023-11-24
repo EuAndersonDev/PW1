3622cbdc1a1169fed8f14ca558aa743c512767fd
Anderson Reis <137122362+AndersonReis04@users.noreply.github.com>
2023-11-23 21:59:12 -0300

    Add files via upload

---
+++ petShop/js's/script.js
@@ -0,0 +1,37 @@
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
+
+
+const toggleBtn = document.querySelector('.toggle_btn')
+const toggleBtnIcon = document.querySelector('.toggle_btn i')
+const dropDownMenu = document.querySelector('.dropdown_menu')
+
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle('open');
+    const isOpen = dropDownMenu.classList.contains('open')
+
+    toggleBtnIcon.classList = isOpen
+    ? 'fa-solid fa-xmark'
+    : 'fa-solid fa-bars'
+}
+function alternarClasse(elementoClicado) {
+    var imagens = document.querySelectorAll('.imagens .img-sup img');
+    for (var i = 0; i < imagens.length; i++) {
+        imagens[i].parentNode.classList.remove('ativo');
+    }
+    elementoClicado.parentNode.classList.add('ativo');
+}
+
